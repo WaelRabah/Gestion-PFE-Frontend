c790bbbdc1401572da5cf8a3dd5812a53b3fa22d
HSM137 <houssemeddine.kacemi@insat.u-carthage.tn>
2020-12-09 19:55:22 +0100

    added routing, lazy loading, guards(not implemented yet)

---
+++ src/app/enseignant/enseignant-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { EnseignantComponent } from './enseignant.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: EnseignantComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class EnseignantRoutingModule { }
