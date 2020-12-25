6cf708063b3dd64179a348946d55262b79a6dde3
ZakariaNaaija <zakarianaaija@gmail.com>
2020-12-25 14:56:17 +0100

    completed 'etudiant' feature

--- src/app/administration/gestion-etudiant/edit/edit.component.ts
+++ src/app/administration/gestion-etudiant/edit/edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { MDBModalRef } from 'angular-bootstrap-md';
 import { Subject } from 'rxjs';
+import { EtudiantService } from '../services/etudiant.service';
 
 @Component({
   selector: 'app-edit',
@@ -10,11 +12,18 @@ import { Subject } from 'rxjs';
 export class EditComponent implements OnInit {
   data:any;
   action: Subject<any> = new Subject();
-
-  constructor(public modalRef: MDBModalRef) {}
-  onYesClick() {
-    this.action.next('yes');
-    this.modalRef.hide();
+  constructor(private etudiantService:EtudiantService,public modalRef: MDBModalRef) {}
+  submitted=false;
+  edit(etudiantForm: NgForm) {
+    etudiantForm.value.role="Etudiant";
+    etudiantForm.value._id=this.data._id;
+    this.submitted=true;
+    etudiantForm.value.username=etudiantForm.value.firstname+etudiantForm.value.lastname;
+    console.log(etudiantForm.value);
+    this.etudiantService.updateEtudiant(etudiantForm.value).subscribe(
+      (etudiant)=>{this.action.next(etudiant);this.modalRef.hide();this.submitted=false;},
+      (error)=>{this.submitted=false;console.log(error);}
+    )
   }
   ngOnInit(){
     console.log(this.data);
---
+++ src/app/administration/gestion-etudiant/services/etudiant.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Etudiant } from '../models/etudiant'
+import { environment } from '../../../../environments/environment'
+@Injectable({
+  providedIn: 'root'
+})
+export class EtudiantService {
+
+  constructor(private http:HttpClient) { }
+  
+  userRoute="utilisateurs/";
+
+  getEtudiants():Observable<Etudiant[]>{
+    return this.http.get<Etudiant[]>(environment.backendUrl+this.userRoute+'Etudiant');
+  }
+  addEtudiant(etudiant:Etudiant):Observable<Etudiant>{
+    return this.http.post<Etudiant>(environment.backendUrl+this.userRoute+"register",etudiant);
+  }
+  updateEtudiant(etudiant:Etudiant):Observable<Etudiant>{
+    return this.http.put<Etudiant>(environment.backendUrl+this.userRoute+etudiant._id,etudiant);
+  }
+  deleteEtudiant(id):Observable<Etudiant>{
+    return this.http.delete<Etudiant>(environment.backendUrl+this.userRoute+id);
+  }
+}
