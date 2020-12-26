f13fc2deb99fc32dc862ba32ed782ea1474b749d
ZakariaNaaija <34416546+ZakariaNaaija@users.noreply.github.com>
2020-12-27 00:29:05 +0100

    Update session.service.ts

--- src/app/administration/session/session.service.ts
+++ src/app/administration/session/session.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Session} from './session.model'
-import { environment } from '../../../../../environments/environment'
+import { environment } from '../../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
