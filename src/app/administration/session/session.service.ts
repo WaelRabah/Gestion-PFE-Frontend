c4677fbf3d57053a3ccc8cd3bb9f901ac2f43c11
ZakariaNaaija <34416546+ZakariaNaaija@users.noreply.github.com>
2020-12-27 00:24:31 +0100

    Update session.service.ts

--- src/app/administration/session/session.service.ts
+++ src/app/administration/session/session.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Session} from './session.model'
+import { environment } from '../../../../../environments/environment'
+
 @Injectable({
   providedIn: 'root'
 })
@@ -14,11 +16,11 @@ export class SessionService {
   ) { }
 
   fetchSessions(){
-    return this.http.get<Session[]>("http://localhost:3000/sessions");
+    return this.http.get<Session[]>(environment.backendUrl+"sessions");
 
   }
   fetchSessionById(id:string){
-    return this.http.get<Session>(`http://localhost:3000/sessions/${id}`);
+    return this.http.get<Session>(`${environment.backendUrl}sessions/${id}`);
 
   }
   getSessionById(index:string) {
@@ -33,7 +35,7 @@ export class SessionService {
   }
 
   storeSession(session: Session){
-    return this.http.post("http://localhost:3000/sessions",session)
+    return this.http.post(environment.backendUrl+"sessions",session)
   }
 
   addSession(session: Session){
@@ -41,7 +43,7 @@ export class SessionService {
     this.sessionChanged.next(this.sessions.slice())
   }
   deleteSession(index:string){
-    this.http.delete("http://localhost:3000/sessions/"+index).subscribe( response => {
+    this.http.delete(environment.backendUrl+index).subscribe( response => {
       this.sessions = this.sessions.filter( element => element._id != index);
       this.sessionChanged.next(this.sessions.slice())
     })
@@ -54,7 +56,7 @@ export class SessionService {
       president: doc.value.president,
       date : doc.value.date
     }
-    this.http.put("http://localhost:3000/sessions/"+index,update).subscribe((data:Session) => {
+    this.http.put(environment.backendUrl+index,update).subscribe((data:Session) => {
       data.date = data.date.slice(0,10);
       let idx = this.sessions.indexOf(this.getSessionById(index))
       this.sessions[idx]= data
