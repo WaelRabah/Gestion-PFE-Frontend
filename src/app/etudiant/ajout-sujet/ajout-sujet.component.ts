682a3327043d6e32c032bcdaf13a66aabed24745
HSM137 <houssemeddine.kacemi@insat.u-carthage.tn>
2020-12-26 14:48:59 +0100

    added 'ajouter sujet étudiant'

--- src/app/etudiant/ajout-sujet/ajout-sujet.component.ts
+++ src/app/etudiant/ajout-sujet/ajout-sujet.component.ts
@@ -1,3 +1,7 @@
+import { ToastrService } from 'ngx-toastr';
+import { AuthentificationService } from 'src/app/services/authentification.service';
+import { EtudiantService } from './../services/etudiant.service';
+import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +11,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AjoutSujetComponent implements OnInit {
 
-  constructor() { }
+  submitted = false;
+
+  fileToUpload : File = null;
+  fileIsValid : boolean = null;
+  fileSizeError : boolean = null;
+  fileTypeError : boolean = null;
+
+  success = false;
+  constructor(
+    private etudiantService: EtudiantService,
+    private authService : AuthentificationService,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    this.submitted = true;
+    const formData = new FormData();
+    formData.append('file',this.fileToUpload,'sujet-pfe-'+this.authService.getUserName());
+    this.etudiantService.ajouterSujet(this.convertToFormData(form)).subscribe({
+      error: (error) => {
+        this.submitted = false;
+        this.toastr.error("Veuillez réssayer ultérieurement",'Une erreur est survenue',{positionClass:'toast-bottom-right'});
+        console.log(error)
+      },
+      complete: () => this.success = true,
+    }
+    )
+  }
+
+  handleFileInput(files: FileList){
+    this.fileToUpload = files.item(0);
+    this.fileTypeError = this.fileToUpload.type != 'application/pdf';
+    this.fileSizeError = this.fileToUpload.size > 20000000;
+    this.fileIsValid = !this.fileSizeError && !this.fileTypeError
+
+  }
+
+  convertToFormData(form:NgForm) {
+    const formData = new FormData();
+    for (var key in form.value){
+      formData.append(key,form.value[key])
+    }
+    formData.set('file',this.fileToUpload,'sujet-pfe-'+this.authService.getUserName());
+
+    return formData;
   }
 
 }
