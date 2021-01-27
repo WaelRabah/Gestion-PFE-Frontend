6e9500cea07d18ce81261ebb50d5ebcbedbf96a8
HSM137 <houssemeddine.kacemi@insat.u-carthage.tn>
2021-01-27 16:43:34 +0100

    added lister encadrements

--- src/app/enseignant/enseignant.service.ts
+++ src/app/enseignant/enseignant.service.ts
@@ -1,3 +1,4 @@
+import { AuthentificationService } from './../services/authentification.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -9,10 +10,15 @@ import { environment } from 'src/environments/environment';
 export class EnseignantService {
 
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private authService: AuthentificationService
   ) { }
 
   suggererSujet(sujet): Observable<any> {
     return this.http.post(environment.backendUrl+'suggest-pfe',sujet);
+  }
+
+  getEncadrement(): Observable<any> {
+    return this.http.get(environment.backendUrl+'pfes/encadrement/'+this.authService.getUserId());
   }
 }
